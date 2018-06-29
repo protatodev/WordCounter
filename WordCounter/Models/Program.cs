2441706a7adada3ee35e6e496d8fc0bd58066011
protatodev <thaddonaghue@yahoo.com>
2018-06-29 08:12:02 -0700

    add initial commit and basic file structure

--- WordCounter/Models/Program.cs
+++ WordCounter/Models/Program.cs
@@ -6,7 +6,7 @@ namespace WordCounter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
---
+++ WordCounter/Models/Words.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordCounter.Models
+{
+    class Words
+    {
+    }
+}
